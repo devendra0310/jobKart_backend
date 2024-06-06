dbe39af9ff212dcfde9322031c88ba5d2e614a6d
Devendra Singh <devendrasingh@Devendras-MacBook-Air.local>
2024-06-06 19:46:43 +0530

    2

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const dbConnect=require('./config/dbConnect');
 dbConnect();
 
 // const PORT=process.env.PORT || 3000; 
-const PORT=3000
+const PORT=process.env.PORT || 3000;
 app.listen(PORT,()=>{
     console.log(`App is listening to ${PORT} port`);
 })
